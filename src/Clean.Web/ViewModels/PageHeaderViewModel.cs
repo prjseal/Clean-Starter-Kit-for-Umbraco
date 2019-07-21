bb287620e82801e8703a99703b9e3ef25f7c9657
Paul Seal <prjseal@gmail.com>
2019-07-21 23:44:39 +0100

    - getting models builder working with intellisense

---
+++ src/Clean.Web/ViewModels/PageHeaderViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Umbraco.Core.Models.PublishedContent;
+
+namespace Clean.Web.ViewModels
+{
+    public class PageHeaderViewModel
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public IPublishedContent BackgroundImage { get; set; }
+        public bool HasSubtitle => !string.IsNullOrWhiteSpace(Subtitle);
+        public bool HasBackgroundImage => BackgroundImage != null;
+
+        public PageHeaderViewModel(string name, string title, string subtitle, IPublishedContent backgroundImage)
+        {
+            Name = name;
+            Title = title;
+            Subtitle = subtitle;
+            BackgroundImage = backgroundImage;
+        }
+    }
+}
