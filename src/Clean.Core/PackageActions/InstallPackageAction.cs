371f7f13482768d55177819dd0a083d01a64e8e5
Paul Seal <prjseal@gmail.com>
2019-08-27 23:18:14 +0100

    - fixed contact template - resized and renamed media

--- src/Clean.Core/PackageActions/InstallPackageAction.cs
+++ src/Clean.Core/PackageActions/InstallPackageAction.cs
@@ -20,21 +20,39 @@ namespace Clean.Core.PackageActions
             var mediaTypeService = Current.Services.MediaTypeService;
             var mediaService = Current.Services.MediaService;
 
-            CreateMediaItem(mediaService, mediaTypeService, -1, "image", new Guid("3c6c415c-35a0-4629-891e-683506250c31"), "1", "/media/ljahypfa/1.jpg", false);
-            CreateMediaItem(mediaService, mediaTypeService, -1, "image", new Guid("5598b628-b390-4532-8bb5-dab06089e9d7"), "2", "/media/f01jqvmq/2.jpg", false);
-            CreateMediaItem(mediaService, mediaTypeService, -1, "image", new Guid("bbf2800f-1cc5-4ea9-8d2e-b33ff1d5efbe"), "3", "/media/bgapfo4f/3.jpg", false);
-            CreateMediaItem(mediaService, mediaTypeService, -1, "image", new Guid("48239f24-1450-49da-9964-934f512dde48"), "4", "/media/ekpdgscq/4.jpg", false);
-            CreateMediaItem(mediaService, mediaTypeService, -1, "image", new Guid("167ee715-53ff-4a8b-ab50-7d630c8448fa"), "5", "/media/cpth2fx5/5.jpg", false);
-            CreateMediaItem(mediaService, mediaTypeService, -1, "image", new Guid("b5279cc9-c438-4b14-a57a-72f97f5527f7"), "6", "/media/z4ocpnjy/6.jpg", false);
-            CreateMediaItem(mediaService, mediaTypeService, -1, "image", new Guid("a61578a3-11bb-4425-9fe1-86a89434e638"), "7", "/media/uv2bljv1/7.jpg", false);
-            CreateMediaItem(mediaService, mediaTypeService, -1, "image", new Guid("da20812c-977d-4707-80d9-f2b1cb185ff1"), "8", "/media/fkadbwt3/8.jpg", false);
-            CreateMediaItem(mediaService, mediaTypeService, -1, "image", new Guid("0521b485-98b6-409d-ad35-8745f3e4287c"), "9", "/media/qo0ccbnw/9.jpg", false);
-            CreateMediaItem(mediaService, mediaTypeService, -1, "image", new Guid("8ac2c7bc-0acb-488e-a4e6-24d9ea5bdff7"), "10", "/media/xldnfcwl/10.jpg", false);
-            CreateMediaItem(mediaService, mediaTypeService, -1, "image", new Guid("13e63ebb-2f82-4e65-84f0-e3b79241a971"), "11", "/media/xu5ome2c/11.jpg", false);
-            CreateMediaItem(mediaService, mediaTypeService, -1, "image", new Guid("dd3840b3-8621-451b-9ee9-01f6ad175ec7"), "12", "/media/wvadrxjb/12.jpg", false);
-            CreateMediaItem(mediaService, mediaTypeService, -1, "image", new Guid("db22bb69-b0ae-44bb-9019-a3e6498d4c25"), "13", "/media/n43dfw3o/13.jpg", false);
-            CreateMediaItem(mediaService, mediaTypeService, -1, "image", new Guid("4281aa58-15c6-41a9-b074-33f40fd09ea9"), "14", "/media/l0vicqwg/14.jpg", false);
-            CreateMediaItem(mediaService, mediaTypeService, -1, "image", new Guid("861ac5a3-aca5-4db8-85f2-a7ad05e0de30"), "15", "/media/uxcbvbvf/15.jpg", false);
+            var sampleFolderId = CreateMediaItem(mediaService, mediaTypeService, -1, "folder",
+                new Guid("9bae5a55-05d4-46a9-b2b3-f1ddb9a2bc38"), "Sample Images", "");
+
+            CreateMediaItem(mediaService, mediaTypeService, sampleFolderId, "image", 
+                new Guid("3c6c415c-35a0-4629-891e-683506250c31"), "Chairs lamps", "/media/ljahypfa/chairs-lamps.jpg", false);
+            CreateMediaItem(mediaService, mediaTypeService, sampleFolderId, "image", 
+                new Guid("5598b628-b390-4532-8bb5-dab06089e9d7"), "Bluetooth white keyboard", "/media/f01jqvmq/bluetooth-white-keyboard.jpg", false);
+            CreateMediaItem(mediaService, mediaTypeService, sampleFolderId, "image", 
+                new Guid("bbf2800f-1cc5-4ea9-8d2e-b33ff1d5efbe"), "Phone pen binder", "/media/bgapfo4f/phone-pen-binder.jpg", false);
+            CreateMediaItem(mediaService, mediaTypeService, sampleFolderId, "image", 
+                new Guid("48239f24-1450-49da-9964-934f512dde48"), "Triangle table chairs", "/media/ekpdgscq/triangle-table-chairs.jpg", false);
+            CreateMediaItem(mediaService, mediaTypeService, sampleFolderId, "image", 
+                new Guid("167ee715-53ff-4a8b-ab50-7d630c8448fa"), "Community front row", "/media/cpth2fx5/community-front-row.jpg", false);
+            CreateMediaItem(mediaService, mediaTypeService, sampleFolderId, "image", 
+                new Guid("b5279cc9-c438-4b14-a57a-72f97f5527f7"), "Skrift at codegarden", "/media/z4ocpnjy/skrift-at-codegarden.jpg", false);
+            CreateMediaItem(mediaService, mediaTypeService, sampleFolderId, "image", 
+                new Guid("a61578a3-11bb-4425-9fe1-86a89434e638"), "Meetup organizers at codegarden", "/media/uv2bljv1/meetup-organizers-at-codegarden.jpg", false);
+            CreateMediaItem(mediaService, mediaTypeService, sampleFolderId, "image", 
+                new Guid("da20812c-977d-4707-80d9-f2b1cb185ff1"), "Umbracoffee codegarden", "/media/fkadbwt3/umbracoffee-codegarden.jpg", false);
+            CreateMediaItem(mediaService, mediaTypeService, sampleFolderId, "image", 
+                new Guid("0521b485-98b6-409d-ad35-8745f3e4287c"), "Codegarden keynote", "/media/qo0ccbnw/codegarden-keynote.jpg", false);
+            CreateMediaItem(mediaService, mediaTypeService, sampleFolderId, "image", 
+                new Guid("8ac2c7bc-0acb-488e-a4e6-24d9ea5bdff7"), "Friendly chair", "/media/xldnfcwl/friendly-chair.jpg", false);
+            CreateMediaItem(mediaService, mediaTypeService, sampleFolderId, "image", 
+                new Guid("13e63ebb-2f82-4e65-84f0-e3b79241a971"), "Say cheese", "/media/xu5ome2c/say-cheese.jpg", false);
+            CreateMediaItem(mediaService, mediaTypeService, sampleFolderId, "image", 
+                new Guid("dd3840b3-8621-451b-9ee9-01f6ad175ec7"), "Front row audience smiles", "/media/wvadrxjb/front-row-audience-smiles.jpg", false);
+            CreateMediaItem(mediaService, mediaTypeService, sampleFolderId, "image", 
+                new Guid("db22bb69-b0ae-44bb-9019-a3e6498d4c25"), "First timers at codegarden", "/media/n43dfw3o/first-timers-at-codegarden.jpg", false);
+            CreateMediaItem(mediaService, mediaTypeService, sampleFolderId, "image", 
+                new Guid("4281aa58-15c6-41a9-b074-33f40fd09ea9"), "24 days people at codegarden", "/media/l0vicqwg/24-days-people-at-codegarden.jpg", false);
+            CreateMediaItem(mediaService, mediaTypeService, sampleFolderId, "image", 
+                new Guid("861ac5a3-aca5-4db8-85f2-a7ad05e0de30"), "Desktop notebook glasses", "/media/uxcbvbvf/desktop-notebook-glasses.jpg", false);
 
             var contentHome = contentService.GetRootContent().FirstOrDefault(x => x.ContentType.Alias == "home");
             if (contentHome != null)
